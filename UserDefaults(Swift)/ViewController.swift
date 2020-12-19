5aa5bb57c986b82deba29669961393a6bd7b333f
SergeyGolenko <1989sergiotay@gmail.com>
2020-12-19 21:00:38 +0200

    создал данные и прикрепил UI

--- UserDefaults(Swift)/ViewController.swift
+++ UserDefaults(Swift)/ViewController.swift
@@ -8,10 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let number = "0502274444"
+    var count = 0
+    
+    func checkPhoneNumber(){
+        if number == phoneNumber.text{
+            count += 1
+            phoneNumber.text = ""
+            print(count)
+        }
+    }
+    
+    
+    @IBOutlet weak var phoneNumber: UITextField!
+    @IBOutlet weak var countsLabel: UILabel!
+    
+    @IBAction func checkButton(_ sender: Any) {
+        checkPhoneNumber()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+         
     }
 
 
